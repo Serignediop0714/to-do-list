2981e8b994818f3a6fd394265973e52d68f06867
Serignediop0714 <122436637+Serignediop0714@users.noreply.github.com>
2023-01-12 16:27:43 +0100

    Add files via upload

---
+++ to do list/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.9.5
+ */
+
+/**
+ * Database `mytodolist`
+ */
+
+/* `mytodolist`.`my to do list` */
+$my_to_do_list = array(
+);
---
+++ to do list/readme.php
@@ -0,0 +1,7 @@
+Pour notre TO DO LIST on doit d'abord créer notre formulaire 
+pour créer le formulaire on va créer notre base de donnée à l'aide de phpmyadmin et du coupon crée les données, les variables et les clés primaires
+ensuite aprés avoir déduis les variables et données on exporte notre fichier à l'aide d'une commande array_php de phpmyadmin ou exporter sous forme de tableau 
+ainsi on a notre base de donnée 
+Après avoir crée  la base de donnée on va structurer notre site 
+pour ca il nous faut donc de l'html et du css 
+pour ca on fait le lien entre l'html et le css et on obtient le site 
